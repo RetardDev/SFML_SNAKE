dd436a59f3173ce1c98401cba21674eb9af8232e
RetardDev <aryptise@gmail.com>
2024-02-10 21:11:26 +0100

    second update

--- main.cpp
+++ main.cpp
@@ -38,6 +38,41 @@ void drawGrid(sf::RenderWindow& win, int rows, int cols){
     // draw it
     win.draw(grid);
 }
+void handleKeyboard(Direction &currentDirection)
+{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && currentDirection != East){currentDirection = West;
+    }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && currentDirection != West){currentDirection = East;
+    }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && currentDirection != South){currentDirection = North;
+    }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && currentDirection != North){currentDirection = South;}
+}
+
+void snakePos(std::vector<sf::RectangleShape> &body, Direction currentDirection, float speed){
+//    sf::Vector2f headPos = body[0].getPosition();
+    sf::Vector2f movement(0.f, 0.f);
+
+    switch(currentDirection){
+        case West:
+            movement.x = -speed;
+            break;
+        case East:
+            movement.x = speed;
+            break;
+        case North:
+            movement.y = -speed;
+            break;
+        case South:
+            movement.y = speed;
+            break;
+        default:
+            break;
+    }
+    body[0].move(movement);
+    for(int i = body.size() - 1; i > 0; i--){
+        body[i].setPosition(body[i - 1].getPosition());
+    }
+}
+
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML SNAKE");
     window.setFramerateLimit(60);
@@ -76,36 +111,16 @@ int main() {
                 window.close();
         }
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-            direction = West;
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-            direction = East;
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            direction = North;
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-            direction = South;
-        }
-        if(direction == West){body[0].move(-speed, 0);
-        }else if(direction == East){body[0].move(speed, 0);
-        }else if(direction == North){body[0].move(0.f, -speed);
-        }else if(direction == South){  body[0].move(0.f, speed);}
+        handleKeyboard(direction);
 
-        for(int i = body.size() - 1; i > 0; i--){
-            sf::Vector2f  prevPos = body[i - 1].getPosition();
-            if(direction == South){body[i].setPosition(prevPos.x, prevPos.y - speed * 2);
-            }else if(direction == North){body[i].setPosition(prevPos.x, prevPos.y + speed * 2);
-            }else if(direction == West){body[i].setPosition(prevPos.x + speed * 2, prevPos.y);
-            }else if(direction == East){body[i].setPosition(prevPos.x -  speed * 2, prevPos.y);}
-        }
+        snakePos(body, direction, speed);
         if(body[0].getGlobalBounds().intersects(food.getGlobalBounds())){
             food.setPosition(xDist(gen), yDist(gen));
             body.push_back(sf::RectangleShape(sf::Vector2f(30.f, 30.f)));
         }
 
-
-
         window.clear();
-//        drawGrid(window, 20, 20);
+       drawGrid(window, 20, 20);
         for(int i = 0; i < body.size(); i++){
             window.draw(body[i]);
         }
